45cbf8127ecc1fac361bafdf9001f643916c1ec0
Ben Coleman <benc.uk@gmail.com>
2020-05-08 11:04:48 +0100

    Fri May  8 11:04:48 BST 2020

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git clone https://github.com/benc-uk/ubuntu-tools-install.git $HOME/setup
