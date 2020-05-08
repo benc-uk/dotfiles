29988d8e33a3040a66eeee3c474102a865a6c3e9
Ben Coleman <benc.uk@gmail.com>
2020-05-08 14:16:47 +0100

    argh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Enable oh-my-zsh and p10k
+if [ -f "/bin/zsh" ]; then
+  echo "### Zsh detected, setting up oh-my-zsh and powerlevel10k"
+  sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+fi
 
 # Create symlinks for all dotfiles and bin directory
 echo "### Creating symlinks"
@@ -8,13 +14,6 @@ do
   ln -s $HOME/.dotfiles/$f $HOME/$f
 done
 exit
-
-# Enable oh-my-zsh and p10k
-if test -f "/bin/zsh"; then
-  echo "### Zsh detected, setting up oh-my-zsh and powerlevel10k"
-  sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-fi
 
 # Clone my setup scripts
 echo "### Cloning tools repo to $HOME/tools"
