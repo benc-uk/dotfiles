5c95d244ddad96fadc573e28a39ec4d4c88083b3
Ben Coleman <benc.uk@gmail.com>
2020-05-08 11:22:05 +0100

    Fri May  8 11:22:05 BST 2020

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ do
   rm $HOME/$f > /dev/null
   ln -s $HOME/.dotfiles/$f $HOME/$f
 done
+
+if test -f "/bin/zsh"; then
+  sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+fi
