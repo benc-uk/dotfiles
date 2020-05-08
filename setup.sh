9e5d4c76905260700f6a74c89da194ea25225a25
Ben Coleman <benc.uk@gmail.com>
2020-05-08 10:14:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ do
   ln -s $HOME/.dotfiles/$f $HOME/$f
 done
 
-sudo apt install zsh
+apt install zsh
 chsh -s /usr/bin/zsh $USER
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
