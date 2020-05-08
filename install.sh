e3ebdfdcf039e3349c4ed43b8e9d4939537be12a
Ben Coleman <benc.uk@gmail.com>
2020-05-08 11:31:27 +0100

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+# Create symlinks for all dotfiles and bin directory
 for f in .zshrc .zshenv .p10k.zsh .gitconfig .profile .bashrc .myaliases .secrets.sh bin
 do
   rm $HOME/$f > /dev/null
   ln -s $HOME/.dotfiles/$f $HOME/$f
 done
 
+# Enable oh-my-zsh and p10k
 if test -f "/bin/zsh"; then
   sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 fi
+
+# Clone my setup scripts
+git clone https://github.com/benc-uk/ubuntu-tools-install.git $HOME/setup
--- setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git clone https://github.com/benc-uk/ubuntu-tools-install.git $HOME/setup
