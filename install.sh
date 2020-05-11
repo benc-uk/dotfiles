6daf7f1ec620d67daa6487bba4033563cae49e7d
Ben Coleman <benc.uk@gmail.com>
2020-05-11 08:48:55 +0100

    Mon May 11 08:48:55 BST 2020

--- install.sh
+++ install.sh
@@ -54,4 +54,4 @@ ln -s $HOME/.dotfiles/.env.rc $HOME/.zshenv
 #
 echo -e "\n\e[38;5;45m»»» Cloning tools repo to $HOME/tools \e[0m"
 rm -rf $HOME/tools
-git clone -q https://github.com/benc-uk/ubuntu-tools-install.git $HOME/tools
+git clone -q https://github.com/benc-uk/tools-install.git $HOME/tools
