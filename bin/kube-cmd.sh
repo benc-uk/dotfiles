4ab605d9bcf0aad9ff35622213ce1b2d0957d849
Ben Coleman <benc.uk@gmail.com>
2021-02-12 11:11:23 +0000

    Fri Feb 12 11:11:23 GMT 2021

---
+++ bin/kube-cmd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+image=${1:-busybox}
+cmd=${2:-ls}
+
+echo "kubectl run test -it --rm --image=$image --restart=Never -- $cmd"
+kubectl run test -it --rm --image=$image --restart=Never -- $cmd
