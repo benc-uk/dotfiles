b786a024b82f89e2d85d1803d90e5ba0080c353f
Ben Coleman <benc.uk@gmail.com>
2021-04-02 21:43:49 +0100

    Fri Apr  2 21:43:49 BST 2021

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo -e "\e[38;5;214m»»» 🙊 If you have anything in these files/folders, pl
 echo -e "\e[38;5;214m»»» 🙈   \e[38;5;227m.zshrc .zshenv .bashenv .p10k.zsh .gitconfig .profile .bashrc ~/bin/ ~/tools/ ~/.oh-my-zsh"
 echo -e "\e[38;5;214m»»» 🐵 Only continue with this script when it is ok to overwrite these files...\n\e[0m"
 
-PROMPT="1"
+PROMPT="0"
 if [[ $1 == "noprompt" ]]; then
   PROMPT="0"
 fi
