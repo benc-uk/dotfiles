2a2391d05ca007c65bc8938c22052a083290c410
Ben Coleman <benc.uk@gmail.com>
2021-04-05 15:15:49 +0100

    Mon Apr  5 15:15:49 BST 2021

--- install-zsh.sh
+++ install-zsh.sh
@@ -4,7 +4,13 @@
 # Simple script to install zsh and try to change default shell to it
 #
 
-sudo apt update -qq
-sudo apt install -y zsh
+if which sudo > /dev/null; then
+  sudo apt update -qq
+  sudo apt install -y zsh
+else
+  apt update -qq
+  apt install -y zsh
+fi
+
 echo "💠💠💠 NOTE! To change the default shell run: chsh -s /usr/bin/zsh \$USER"
 echo "💠💠💠 NOTE! This may prompt for a password 😥"
