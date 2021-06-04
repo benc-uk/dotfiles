ce882882534a7464cdb2d8eca794551076b2dfb3
Ben Coleman <benc.uk@gmail.com>
2021-06-04 11:11:26 +0100

    🍉 Fri Jun  4 11:11:26 BST 2021

--- bin/az-clean-temp.sh
+++ bin/az-clean-temp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-groups=`az group list --query "[].name" -o tsv`
+groups=$(az group list --query "[].name" -o tsv)
+wildcard=${1:-foo}
 delGroups=()
 for group in $groups; do
-  if [[ "${group,,}" =~ .*"temp".* ]]; then
+  if [[ "${group,,}" =~ .*"$wildcard".* ]]; then
     delGroups+="$group,"
   fi
 done
@@ -11,7 +12,7 @@ done
 IFS=$','
 for group in $delGroups; do
   echo -e "\e[31m»»» 💥 Going to delete resource group: \e[0m$group"
-  read -n 1 -s -r -p $'Press any key to continue, or ctrl-c to exit\n'
+  #read -n 1 -s -r -p $'Press any key to continue, or ctrl-c to exit\n'
   echo -e "\e[33m»»» 👋 az group delete -g $group --no-wait --yes"
   az group delete -g $group --no-wait --yes
 done
