9f232d76c0657ffc8ab44e4b923b72714b274d65
Ben Coleman <benc.uk@gmail.com>
2022-01-07 11:11:47 +0000

    🍉 Fri Jan  7 11:11:47 GMT 2022

--- .p10k.zsh
+++ .p10k.zsh
@@ -353,7 +353,8 @@
   #####################################[ vcs: git status ]######################################
   # Version control system colors.
   # typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=2
-  # typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=3
+  # This looks better with new W10 emoji
+  typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=172
   # typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
   # typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=3
   # typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
